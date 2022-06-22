6b56ecc3acff195f5e0ce4ca74ca1a47b6b04078
fransalfiando24 <fransalfiando@gmail.com>
2022-06-22 11:25:16 +0700

    firts

---
+++ src/App.js
@@ -0,0 +1,46 @@
+import {BrowserRouter as Router, Link} from 'react-router-dom'
+import styled from 'styled-components'
+import {GiKnifeFork} from 'react-icons/gi'
+import Pages from './pages/Pages'
+import Category from './components/Category'
+import Search from './components/Search'
+
+function App() {
+  return (
+    <div className="App">
+      <Router>
+        <Nav> 
+          <GiKnifeFork/>
+          <Logo to='/'>Let's Cook Together</Logo>
+          <GiKnifeFork/>
+        </Nav>
+        <Category/>
+        <Search/>
+        <Pages/>
+      </Router>
+    </div>
+  );
+}
+
+const Logo = styled(Link)`
+  text-decoration: none;
+  font-size: 1.5rem;
+  font-weight: 400;
+  font-family: 'Lobster Two', cursive;
+`
+
+
+const Nav = styled.div`
+  padding: 2rem 0;
+  width : 100%;
+  height : 50px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  svg{
+    font-size:  2rem;
+    margin: 0 1rem;
+  }
+`
+
+export default App;
